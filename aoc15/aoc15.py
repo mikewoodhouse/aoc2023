45ae9e1b417465285d06c275c7f7460a0626ad05
Mike Woodhouse <mikewoodhouse@gmail.com>
2024-01-27 12:42:22 +0000

    aoc2023/15/2 done

--- aoc15/aoc15.py
+++ aoc15/aoc15.py
@@ -1,5 +1,6 @@
 from __future__ import annotations
 
+from collections import defaultdict
 from dataclasses import dataclass
 
 from icecream import ic
@@ -25,9 +26,53 @@ def hash_total(p: list[str]) -> int:
     return tot
 
 
+@dataclass()
+class Step:
+    label: str
+    op: str
+    length: int
+
+    def apply_to(self, boxes: dict[int, list[Step]]) -> None:
+        boxno = hash(self.label)
+        if self.op == "=":
+            replaced = False
+            for lens in boxes[boxno]:
+                if lens.label == self.label:
+                    lens.length = self.length
+                    replaced = True
+            if not replaced:
+                boxes[boxno].append(self)
+        else:
+            target = next(
+                (idx for idx, lens in enumerate(boxes[boxno]) if lens.label == self.label),
+                -1,
+            )
+            if target == -1:
+                return
+            del boxes[boxno][target]
+
+    @staticmethod
+    def from_string(s: str) -> Step:
+        if "=" not in s:
+            return Step(s[:-1], "-", 0)
+        t = s.split("=")
+        return Step(t[0], "=", int(t[1]))
+
+    def __repr__(self) -> str:
+        return f"{self.label}{self.op}{'' if self.op == '-' else str(self.length)}"
+
+
+def box_power(boxno: int, lenses: list[Step]) -> int:
+    return sum((1 + boxno) * slotno * step.length for slotno, step in enumerate(lenses, 1))
+
+
 def part1(lines: list[str]) -> int:
     return hash_total(lines)
 
 
 def part2(lines: list[str]) -> int:
-    return 0
+    boxes: dict[int, list] = defaultdict(list)
+    steps = [Step.from_string(s) for s in lines]
+    for step in steps:
+        step.apply_to((boxes))
+    return sum(box_power(boxno, lenses) for boxno, lenses in boxes.items())
--- tests/test_aoc15.py
+++ tests/test_aoc15.py
@@ -1,7 +1,10 @@
+from collections import defaultdict
+
 import pytest
+from icecream import ic
 
 from aoc15 import part1, part2
-from aoc15.aoc15 import hash, hash_total
+from aoc15.aoc15 import Step, box_power, hash, hash_total
 from lib import single_line
 
 
@@ -16,3 +19,30 @@ def test_single_line():
 
 def test_sample_hash(sample):
     assert hash_total(sample) == 1320
+
+
+def test_step_create():
+    assert Step.from_string("gblm=9") == Step("gblm", "=", 9)
+    assert Step.from_string("rbv-") == Step("rbv", "-", 0)
+
+
+def test_box_power():
+    boxes = {
+        0: [Step("", "", 1), Step("", "", 2)],
+        3: [Step("", "", 7), Step("", "", 5), Step("", "", 6)],
+    }
+    assert sum(box_power(i, b) for i, b in boxes.items()) == 145
+
+
+def test_apply_step(sample):
+    steps = [Step.from_string(s) for s in sample]
+    boxes = defaultdict(list[Step])
+    for step in steps:
+        step.apply_to(boxes)
+    assert sum(len(box) > 0 for box in boxes.values())
+    assert boxes[0] == [steps[0], steps[3]]
+    assert boxes[3] == [steps[10], steps[7], steps[9]]
+
+
+def test_part2(sample):
+    assert part2(sample) == 145
